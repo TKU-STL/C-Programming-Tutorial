aa4f402c4fae6e948e46e249d2d5b3c6fdde7389
林仁奕 <119604228+Renyilin0115@users.noreply.github.com>
2022-12-21 00:04:30 +0800

    Add files via upload

---
+++ HW3-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int list[] = {1,4,2,4,6,78,4,3,2,4};
+
+    int listLength = sizeof(list)/sizeof(int);
+    
+    int i = 0;
+    
+    while (i <= listLength - 1){
+        printf("%d,", list[i]);
+        i++;
+    }
+  return 0;
+  
+  }
