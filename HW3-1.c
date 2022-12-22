beeee93c096d522c6a18c11e10b9c7f71b63a9a2
林仁奕 <119604228+Renyilin0115@users.noreply.github.com>
2022-12-22 16:07:20 +0800

    Update HW3-1.c

--- HW3-1.c
+++ HW3-1.c
@@ -3,12 +3,12 @@
 
 int main(){
 
-    int list[] = {1,4,2,4,6,78,4,3,2,4};
+    int list[] = {1,4,2,4,6,78,4,3,2,4};//建立資料庫
 
-    int listLength = sizeof(list)/sizeof(int);
+    int listLength = sizeof(list)/sizeof(int);//設定Stack大小
     
      while ( listLength != 0){
-        printf("%d,", list[listLength - 1]);
+        printf("%d,", list[listLength - 1]);//輸出資料
         listLength--;
     }
   
