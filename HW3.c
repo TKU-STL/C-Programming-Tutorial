309959216d89253c4b5eb23b16b8f14e17021bd0
JunJun0918 <119605958+JunJun0918@users.noreply.github.com>
2022-12-22 18:28:15 +0800

    Update HW3.c

--- HW3.c
+++ HW3.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 
 int main(void){
-	int var = 100;
+	int var = 100; //var是100
 
 	if(var != 0){
 		printf("The answer is not a zero\n");
-	}
+	}  //先執行 var是不等於0，如果不等於零，會輸出 The answer is not a zero
     if (var > 0){
 			printf("The answer is greater than zero\n");
-	}
+	}  //之後再來執行 var大於0，如果大於零，會輸出 The answer is greater than zero
 	
     
 
