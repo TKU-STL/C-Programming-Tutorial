6542710a7c6f6a7194781506de186fd2bc06f9ef
JunJun0918 <119605958+JunJun0918@users.noreply.github.com>
2022-12-22 18:26:40 +0800

    Update HW1.c

--- HW1.c
+++ HW1.c
@@ -4,14 +4,14 @@
 
 void main(void){
 
-    int list[] = {1,4,2,4,6,78,4,3,2,4};
+    int list[] = {1,4,2,4,6,78,4,3,2,4}; //建立矩陣
 
-    int listLength = sizeof(list)/sizeof(int);
+    int listLength = sizeof(list)/sizeof(int); //知道list數字有多少
     
     while (listLength != 0){
         printf("%d,", list[listLength - 1]);
         listLength--;
-    }
+    } //開始while迴圈，輸出 list，listLength - 1 是從後面開始一一輸出
     
     return 0;
 }
